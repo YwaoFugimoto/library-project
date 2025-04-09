008b6720f89a502bb4ffaf4a51b9ca297180cef1
YwaoFugimoto <pedro.fugimoto@sou.unifal-mg.edu.br>
2025-04-09 14:41:43 -0300

    feat: Adding pre remove config

--- models/author.js
+++ models/author.js
@@ -7,4 +7,49 @@ const authorSchema = new mongoose.Schema({
     }
 })
 
+// authorSchema.pre('deleteOne', function(next) {
+//     const query = this.getFilter()
+//     const Book = require('./book')
+//     const books = Book.find({ author: query._id})
+//     Book.exists({ author: this.id }, (err, books) => {
+//         if( err ){
+//             next(err)
+//         } else if (books.length > 0) {
+//             next(new Error('This author has books still'))
+//         } else {
+//             next ()
+//         }
+//     })
+// })
+
+authorSchema.pre("deleteOne", async function (next) {
+    try {
+        const query = this.getFilter();
+        const Book = require('./book'); 
+        const books = await Book.find({ author: query._id });
+        if (books.length > 0) {
+            next(new Error("This author still has books."));
+        } else {
+            next();
+        }
+    } catch (err) {
+        next(err);  
+    }
+});
+
+// authorSchema.pre("deleteOne", async function (next) {
+//     try {
+//         const query = this.getFilter();
+//         const Book = require('./book')
+//         const hasBook = await Book.find({ author: query._id });
+//         if (hasBook) {
+//             next(new Error("This author still has books."));
+//         } else {
+//             next();
+//         }
+//     } catch (err) {
+//         next(err);
+//     }
+// });
+
 module.exports = mongoose.model('Author', authorSchema)
